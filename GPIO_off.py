7b6ecba31fc145c54c83a05553bec15382907d3e
Thatdot7 <wanmoses1@gmail.com>
2013-03-15 10:52:35 +1100

    Added scripts to manually operate GPIO pins

---
+++ GPIO_off.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import sys
+
+#####################################################################################################################
+# GPIO_off
+#
+# This script is only meant to push a particular pin in the GPIO to its LOW state.
+# Before use, please read about the particular functions of each GPIO pin to ensure that it is usable for you
+#####################################################################################################################
+
+
+def run_script(pin):
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(int(pin), GPIO.OUT)
+	GPIO.output(int(pin), GPIO.LOW)
+
+	print "Pin " + pin + " is now low"
+
+if __name__ == "__main__":
+	run_script(sys.argv[1])
+
