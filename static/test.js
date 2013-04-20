b8fab2c64db957f201c5f1420ac2c6d9da4f392f
Thatdot7 <wanmoses1@gmail.com>
2013-04-21 00:42:27 +1000

    Linux Recovery 2

---
+++ static/test.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){	
+	SocketHandler.start();
+	
+	$(".on-button, .off-button").on('click', function(){
+		var message = $(this).data("plug");
+		SocketHandler.socket.send(message);
+	});
+});
+
+var SocketHandler = {
+	socket: null,
+	
+	start: function() {
+		var url = "ws://" + location.host + "/ws";
+		if ("WebSocket" in window) {
+			SocketHandler.socket = new WebSocket(url);
+		} else {
+			SocketHandler.socket = new MozWebSocket(url);
+		}
+		SocketHandler.socket.onmessage = function(event) {
+			var data = event.data;
+			if(data.charAt(0) == "1"){
+				$("#light1").attr("src","static/images/images.png");
+			} else {
+				$("#light1").attr("src","static/images/images2.png");
+			}
+			if(data.charAt(1) == "1"){
+				$("#light2").attr("src","static/images/images.png");
+			} else {
+				$("#light2").attr("src","static/images/images2.png");
+			}
+			if(data.charAt(2) == "1"){
+				$("#light3").attr("src","static/images/images.png");
+			} else {
+				$("#light3").attr("src","static/images/images2.png");
+			}
+			if(data.charAt(3) == "1"){
+				$("#light4").attr("src","static/images/images.png");
+			} else {
+				$("#light4").attr("src","static/images/images2.png");
+			};
+		}
+	}
+}
